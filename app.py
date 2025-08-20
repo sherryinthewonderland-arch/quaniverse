6421822f600b6e640d3d2b0aba164c4656231d33
sherryinthewonderland-arch <sherryinthewonderland@gmail.com>
2025-08-20 01:15:28 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+import os
+
 app = Flask(__name__, template_folder='.')
 
 @app.route('/')
@@ -13,5 +15,7 @@ def page(page):
         return "404 Not Found", 404
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3000)
+    port = int(os.environ.get('PORT', 3000))
+    app.run(host='0.0.0.0', port=port)
+
 
